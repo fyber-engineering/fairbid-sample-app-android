834ef77e501c44e5841f358cdd86a508298b7f95
Sagi Antebi <sagi.antebi@fyber.com>
2019-03-17 14:22:29 +0200

    Initial commit of a new Android project with some basic settings

---
+++ app/src/main/java/com/fyber/fairbid/sample/MainActivity.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019. Fyber N.V
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fyber.fairbid.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * The Main Activity,
+ * responsible for starting the FairBid SDK and displaying the different examples -
+ *
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
